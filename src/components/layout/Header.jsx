8a051beae45171ca390b1762b8f65fdc15d38a5c
VinciVee <vincent@DESKTOP-O7AGANI.>
2025-06-24 18:07:45 +1000

    updated Nav bar: added hover and resized icon

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,48 +7,24 @@ import { GiFoxTail } from "react-icons/gi"
 const Header = () => {
   return (
     <nav className="text-indigo-300 bg-gray-900">
-      <div className="flex flex-row flex-wrap items-center justify-between h-[3em] mx-[5vw]">
+      {/* Nav container */}
+      <div className="container flex flex-row flex-wrap items-center justify-between mx-[5vw]">
+
         {/* LEFT: BRAND CHILD */}
-        <Link to="/">
-          <GiFoxTail className="inline mr-2"/>
-          <span>PortfolioHome</span>
+        <Link to="/" className="p-3 cursor-pointer size-fit hover:text-blue-300">
+          <GiFoxTail className="inline mr-3 text-2xl"/>
+          <span>Portfolio Home</span>
         </Link>
+
         {/* RIGHT: NAVLINKS CHILD */}
         <div className="size-fit flex flex-row items-baseline gap-[4vw] flex-nowrap" >
-          <div>
-            <Link to="/About">About</Link>
-          </div>
-          <div>
-            <Link to="/Contact">Contact</Link>
-          </div>
-          <div>
-            <Link to="/Projects">Projects</Link>
-          </div>
+          <div className="p-3 cursor-pointer hover:text-blue-300"><Link to="/About">About</Link></div>
+          <div className="p-3 cursor-pointer hover:text-blue-300"><Link to="/Contact">Contact</Link></div>
+          <div className="p-3 cursor-pointer hover:text-blue-300"><Link to="/Projects">Projects</Link></div>
         </div>
       </div>
     </nav>
   )
 }
-
-// const HeaderBootstrap = () => {
-//   return (
-//     <Navbar expand="lg" className="bg-body-tertiary">
-//       <Container>
-//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="me-auto">
-//             {/* 
-//             <Nav.Link href="#about">About</Nav.Link>
-//             <Nav.Link href="#link">Link</Nav.Link>
-//             */}
-//             <Nav.Link as={Link} to="/">Home</Nav.Link>
-//             <Nav.Link as={Link} to="/about">About</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   )
-// }
 
 export default Header
