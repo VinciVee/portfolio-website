11da946b5fb9116ada46791f61afa0a35b841ce9
VinciVee <vincivee@users.noreply.github.com>
2026-06-24 14:15:02 +1000

    updated layout and header layout

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,31 +4,36 @@ import { Link, NavLink } from 'react-router';
 const Header = () => {
 
   return (
-    <header className='w-1/2'>
-      <div className='px-2 flex flex-col items-start justify-start mx-auto'>
-        {/* Logo */}
-        <Link
-          to='/'
-          className='text-2xl font-semibold'
-        >
-          Vincent Veerapen
-        </Link>
+    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between">
+      <div>
+        <h1 className="">
+          <Link to="/">
+            Vincent Veerapen
+          </Link>
+        </h1>
+        <h2>Web Developer</h2>
+        <p>Description: what do I do?</p>
 
-        {/* Links */}
-        <nav className='w-auto' aria-label='Main Navigation'>
-          <ul className='flex flex-col justify-around items-start space-x-6 py-6'>
+        {/* Navigation */}
+        <nav className="hidden lg:block" aria-label="In-page jump links">
+          <ul className="flex flex-col justify-around items-start space-x-6 py-6">
             <li>
-              <NavLink to='/about' className='text-lg'>about</NavLink>
+              <NavLink to="/about" className="text-lg">about</NavLink>
             </li>
             <li>
-              <NavLink to='/projects' className='text-lg'>projects</NavLink>
+              <NavLink to="/projects" className="text-lg">projects</NavLink>
             </li>
             <li>
-              <NavLink to='/resume' className='text-lg'>resume</NavLink>
+              <NavLink to="/resume" className="text-lg">resume</NavLink>
             </li>
           </ul>
         </nav>
       </div>
+
+      {/* social media links */}
+      <ul>
+
+      </ul>
     </header>
   )
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -9,9 +9,9 @@ const Layout = () => {
 
   return (
     // <div className={"app " + theme}>
-    <div className="min-h-screen flex flex-row max-w-7xl pbs-8 pbe-16 mx-auto">
+    <div className="min-h-screen lg:flex lg:justify-between lg:gap-4 max-w-7xl px-6 py-12 mx-auto font-display md:px-12 md:py-16 lg:py-24">
       <Header />
-      <main className="w-1/2">
+      <main className="lg:w-[52%]">
         <Outlet />
       </main>
       {/* <Footer /> */}
