08ae734ff2b9bb72426e866dea85fd2664c1e865
VinciVee <vincivee@users.noreply.github.com>
2026-07-28 11:34:56 +1000

    updated ImageStack layout; also added comments for transform values

--- src/components/features/card/ImageStack.jsx
+++ src/components/features/card/ImageStack.jsx
@@ -2,29 +2,34 @@
 
 function ImageStack({name, images}) {
   const imageArray = Object.entries(images)
+  const opacityStyles = ['opacity-100', 'opacity-80', 'opacity-60', 'opacity-40']
 
   return (
-    <div className="flex justify-start h-38 w-full mb-2 bg-black/25 rounded-lg py-1 px-2">
+    <div className="flex justify-start h-38 max-w-85 mb-2 overflow-visible perspective-distant perspective-origin-center">
       {Object.entries(images)
         .map(([key, value], index) => (
-        // Control amount of each image shown, and
-        // Grey overlay/background
-        <div
-          key={`${name}-${key}`}
-          style={{
-            flex: `0 ${index} content`,
-          }}
-          className="drop-shadow-lg min-w-4"
-        >
-          <img
-            loading="lazy"
-            height="167"
-            width="250"
-            src={value}
-            alt={`${name} ${key} screenshot`}
-            className="h-full object-cover object-top-right"
-          />
-        </div>
+          // Control amount of each image shown, and
+          // Grey overlay/background
+          // transform: `translateX(${index * -8}px) rotateX(6deg) rotateY(-18deg) rotateZ(1deg)`,
+          // transformOrigin: "left center",
+          <div
+            key={`${name}-${key}`}
+            style={{
+              flex: `0 ${index} content`,
+              zIndex: imageArray.length - index,
+              minWidth: `${16 / index}%`
+            }}
+            className="drop-shadow-[3px_10px_8px_rgba(0,0,0,0.25)] min-w-2 bg-gray-500"
+          >
+            <img
+              src={value}
+              alt={`${name} ${key} screenshot`}
+              loading="lazy"
+              height="152"
+              width="228"
+              className={`h-full object-cover object-top-right ${opacityStyles[index] || 'opacity-20'}`}
+            />
+          </div>
       ))}
     </div>
   )
