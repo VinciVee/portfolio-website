571cd743de8499c527d7220ffdd71ba81b9a4891
VinciVee <vincivee@users.noreply.github.com>
2026-08-07 18:37:00 +1000

    updating About text

--- src/pages/sections/About.jsx
+++ src/pages/sections/About.jsx
@@ -7,10 +7,10 @@ function About() {
       <h3 className="lg:hidden">About</h3>
       <div className="flex flex-col gap-6">
         <p>
-          Heya there, I’m Vincent. I'm a recently graduated web developer with an interest in building websites that are both accessible and reliable. I'm driven by projects combining technical challenges, distinctive visual themes and real-life applications. With a background in quality and engineering, I'm searching for an opportunity to put my attention to detail, technical skills, and creativity to work.
+        Hey there, I’m Vincent. I’m a web development graduate who builds website with character and robust designs; hoping to make them more accessible over time.  With a background in quality and engineering, I'm searching for an opportunity to put my attention to detail, technical skills, and creativity to work.
         </p>
         <p>
-          Previously, I completed a Bachelor of Engineering in Mechatronics—split between Swinburne's Kuching and Melbourne campuses. I then worked as a Computer System Validations (CSV) Engineer at <a href="https://4dmedical.com/">4DMedical</a>, a medical device company. Within this highly-regulated industry, I worked closely with internal teams to ensure that their software met quality and regulatory standards. I take pride in the documentation and testing skills, as well as the quality-focused mindset I've developed in that role.
+          Previously, I completed a Bachelor of Engineering in Mechatronics; split between Swinburne's Kuching and Melbourne campuses. I then worked as a Computer System Validations (CSV) Engineer at <a href="https://4dmedical.com/">4DMedical</a>, a medical device company. Within this highly-regulated industry, I worked closely with internal teams to ensure that their software met quality and regulatory standards. I take pride in the documentation and testing skills, as well as the quality-focused mindset I've developed in that role.
         </p>
         <p>
           Currently, I'm looking for a role in web development whilst working on this site and polishing previous projects. My previous experiences, background and creative drive have led me to this career choice: to transition to building web applications which require documentation, coding, design and quality.
